8eb943966ccf6a5f62f9f5d84d906d9c447ee60f
bestsort <bestsort126@gmail.com>
2021-05-28 19:52:31 +0800

    0

--- static/js/config.js
+++ static/js/config.js
@@ -1,6 +1,34 @@
 
+let domain = "http://47.95.238.136:5000";
+//domain = "localhost"
+
+let default_val = {
+    "hight" : 100,
+    "weight" : 50,
+    "count" : 10,
+    "_format" : "jpg"
+}
 let generate_samples = {
     "generate_samples_url":"",
-    "download_samples_url":"http://47.95.238.136:5000/static/resources/captcha_download.zip",
-    "submit_identification" : "http://47.95.238.136:5000/index.html"
+    "download_samples_url":"/download",
+    "submit_identification" : "/index.html"
+}
+
+
+function getUrl(key) {
+    return domain + generate_samples[key];
+}
+function getUrlWithoutDomain(key) {
+    return generate_samples[key]
+}
+
+function download(url)
+{
+    var a = document.createElement('a');
+    a.href = url;
+    a.target = "_blank"
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+//下载文件的地址
 }
--- static/js/event.js
+++ static/js/event.js
@@ -1,22 +1,36 @@
 $('#generate_samples').click(function () {
-    let hight = $('generator_samples_high').val();
-    let weight = $('generator_samples_wight').val();
-    let count = $('generator_samples_count').val();
-    ajax_post(generate_samples["generate_samples_url"],
-        {
-            "high" : hight,
-            "weight" : weight,
-            "count" : count
-        }
-    );
-    οnclick=window.location.href=generate_samples["download_samples_url"]
-    ajax_get(generate_samples["download_samples_url"],
-        function (data) {
-        })
+    let hight = $('#generator_samples_high').val();
+    let weight = $('#generator_samples_wight').val();
+    let count = $('#generator_samples_count').val();
+    let _format = $('#generator_samples_format').val();
+    let fdefault;
+
+    if (hight === "") {
+        hight = default_val["hight"];
+        fdefault = true;
+    }
+    if (weight === "") {
+        weight = default_val["weight"];
+        fdefault = true;
+    }
+    if (count === "") {
+        count = default_val["count"];
+        fdefault = true;
+    }
+    if (_format === "") {
+        _format = default_val["_format"];
+    }
+    let url = getUrl("download_samples_url")
+        + "?high=" + hight
+        + "&width=" + weight
+        + "&count=" + count
+        + "&fault=" + fdefault
+        + "&format=" + _format;
+    download(url)
 });
 
 $('#view_samples').hover(function () {
-    ajax_get(generate_samples["view_samples_url"], null,
+    ajax_get(getUrl("view_samples_url"), null,
         function (data) {
             console.log(111)
         })
@@ -24,13 +38,13 @@ $('#view_samples').hover(function () {
 
 $('#submitIdentification').click(function () {
     var dataObject = new FormData();
-    let url=generate_samples["submit_identification"];
+    let url=getUrl("submit_identification");
     let  file = $("#submitIdentificationFile")[0].files[0];
 
     dataObject.append("file", file);
     let xhr = new XMLHttpRequest();
 
-    xhr.open("post", generate_samples["submit_identification"], true);
+    xhr.open("post", getUrl("submit_identification"), true);
 
     // xhr.onload = function () {
     //
